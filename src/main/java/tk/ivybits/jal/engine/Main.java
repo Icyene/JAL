dbc0e90ce90b7342050d50a1f6cdd3cd0bd70bf3
Tudor <vulcainus@gmail.com>
2013-10-02 19:12:08 -0400

    Do not hardcode name

--- src/main/java/tk/ivybits/jal/engine/Main.java
+++ src/main/java/tk/ivybits/jal/engine/Main.java
@@ -2,26 +2,19 @@ package tk.ivybits.jal.engine;
 
 import tk.ivybits.jal.engine.model.MemoryModel;
 import tk.ivybits.jal.engine.model.MemoryModelFactory;
-import tk.ivybits.profiler.Profiler;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.util.Random;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Tudor
- * Date: 01/10/13
- * Time: 12:39 PM
- * To change this template use File | Settings | File Templates.
- */
 public class Main {
     public static void main(String[] args) throws Exception {
+        String user = System.getProperty("user.name");
         MemoryModel mem = MemoryModelFactory.fromDir(new File("C:/Users/Tudor/Desktop/data"));
 
         String input;
-        System.out.print("Tudor: ");
+        System.out.printf("%s: ", user);
         while ((input = new BufferedReader(new InputStreamReader(System.in)).readLine()) != null) {
             input = input.toUpperCase();
             if (input.contains("BYE")) {
@@ -29,18 +22,14 @@ public class Main {
                 break;
             }
             String[] responses = mem.search(input.toUpperCase());
-            System.out.print("HAL: ");
+            System.out.print("JAL: ");
             if (responses.length == 0) {
                 System.out.println("Sorry, my brain exploded.");
             } else {
                 System.out.println(responses[new Random().nextInt(responses.length)]);
             }
 
-            System.out.print("\nTudor: ");
-            if (!Profiler.isRunning()) {
-                Profiler.start();
-            }
+            System.out.printf("\n%s: ", user);
         }
-        Profiler.stop();
     }
 }
